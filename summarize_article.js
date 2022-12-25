19f2783ec2a43f10993e6a7ee4698116827899af
Daniel Singer <zigius@gmail.com>
2022-12-25 15:49:34 +0200

    Add method to set api key

--- summarize_article.js
+++ summarize_article.js
@@ -3,30 +3,42 @@
 "use strict";
 import * as dotenv from "dotenv";
 dotenv.config();
+import Configstore from "configstore";
 import yargs from "yargs";
 import { Configuration, OpenAIApi } from "openai";
+const config = new Configstore("summarize-article");
 
-const argv = yargs(process.argv.slice(2)).command(
-  "summarize",
-  "summarize article",
-  () => {},
-  async (argv) => {
-    const configuration = new Configuration({
-      apiKey: process.env.OPENAPI_ACCESS_KEY,
-    });
-    const openai = new OpenAIApi(configuration);
+const argv = yargs(process.argv.slice(2))
+  .command(
+    "set-api-token",
+    "set-api-token",
+    () => {},
+    async (argv) => {
+      config.set({ apiKey: argv["_"][1] });
+    }
+  )
+  .command(
+    "summarize",
+    "summarize article",
+    () => {},
+    async (argv) => {
+      const apiKey = config.get("apiKey");
+      const configuration = new Configuration({
+        apiKey: apiKey,
+      });
+      const openai = new OpenAIApi(configuration);
 
-    const prompt = `Please extract 10 bullet points from the following article: ${argv["_"][1]}`;
+      const prompt = `Please extract 10 bullet points from the following article: ${argv["_"][1]}`;
 
-    const model = "text-davinci-003";
+      const model = "text-davinci-003";
 
-    const response = await openai.createCompletion({
-      prompt,
-      model,
-      temperature: 0.5,
-      max_tokens: 250,
-    });
+      const response = await openai.createCompletion({
+        prompt,
+        model,
+        temperature: 0.5,
+        max_tokens: 250,
+      });
 
-    console.log(response.data.choices[0].text);
-  }
-).argv;
+      console.log(response.data.choices[0].text);
+    }
+  ).argv;
